09c10fa12155913c9f29e85e82d956446c9f5706
Saehanseul <saehanseul@gmail.com>
2024-02-16 16:51:42 +0900

    Feat: 6장 거의 끝

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 
+import Button from './Button'
+import styles from './App.module.css'
+import { useEffect, useState } from 'react';
+
+
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [keyword, setKeyword] = useState()
+  const onClick = () => setCounter(prev => prev + 1)
+  const onChange = (e) => setKeyword(e.target.value)
+  console.log("render app")
+
+  const IRunOnlyOnce = () => {
+    console.log("I run only once")
+  }
+  useEffect(IRunOnlyOnce, [])
+
+  useEffect(() => {
+    console.log("search", keyword)
+  }, [keyword])
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <input value={keyword} onChange={onChange} type="text" placeholder='search somethine..'/>
+      <h1 className={styles.title}>{counter}</h1>
+      <Button onClick={onClick} text={"HI~~!"}/>
     </div>
   );
 }
